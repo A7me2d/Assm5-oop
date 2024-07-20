d109b4144751c8307a4ae0b261b6c7f29f5c0b9a
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-20 19:59:39 +0300

    Binary

--- Assm5 oop/Program.cs
+++ Assm5 oop/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace Assm5_oop
+﻿using Assm5_oop.operter_Overloading;
+
+namespace Assm5_oop
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region Opertaor Over LOading
+
+
+
+            Complex C1 = new Complex() { Real = 3, Imag = 5 };
+
+            Console.WriteLine(C1);
+
+            Complex C2 = new Complex() { Real = 2, Imag = 4 };
+
+            Console.WriteLine(C2);
+
+
+            #region Binary
+
+            //Complex C3 = C1 + C2;
+
+            //Console.WriteLine("============");
+
+            //Console.WriteLine(C3);
+
+            //Complex C3 = C1 - C2;
+            //Console.WriteLine("============");
+
+            //Console.WriteLine(C3);
+
+
+            //Complex C3 = default;
+            //C3 += C1;
+
+            //Console.WriteLine(C3);
+            #endregion
+
+
+
+            #endregion
+
+
+
+
         }
     }
 }
---
+++ Assm5 oop/operter Overloading/Complex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assm5_oop.operter_Overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+
+        public int Imag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i" ;
+        }
+
+
+        #region Operators Over
+
+        #endregion
+        public static Complex operator +(Complex Left, Complex Right)
+        {
+            return new Complex 
+            { 
+                Real = (Left?.Real?? 0 ) +( Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) + (Right?.Imag ?? 00 )
+            
+            };
+        }
+
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+            return new Complex
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) - (Right?.Imag ?? 0)
+            };
+        }
+    }
+}
